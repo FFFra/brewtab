8785b8a2d968d2745e3404fde923d240606854e0
Fraschetti <flaviofrasquetiferreira@fraschetti-3.local>
2017-11-02 14:38:40 -0200

    import proptypes module (required)

--- src/App.js
+++ src/App.js
@@ -1,59 +1,82 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 // import {Grid, Jumbotron} from 'react-bootstrap';
 
 class App extends Component {
+   constructor(props) {
+    super(props);
+  }
+
+ 
+
   render() {
     return (
-      <div className="tab">
-        <div className="header">
-          <h1>Viella Tab App</h1>
-        </div>
+      
+        <div className="tab">
+          
+          <Header title={this.props.title}/>
 
-      <div className="total-container">
-        <div className="total-text">Total</div>  
-      </div>  
+          <div className="total-container">
+            <div className="total-text">Total</div>  
+          </div>  
 
-        <div className="customers">
-          <div className="customer">
-            <div className="customer-name">
-             Frá
-            </div>
-            <div className="customer-score">
-              <div className="counter">
-                <button className="counter-action decrement">-</button>
-                  <div className="counter-score"> 31 </div>
-                <button className="counter-action increment">+</button>
-              </div>           
-            </div>
-            <div className="amount-field">
-              <div className="amount-value"> R$43,00</div>
-            </div>
-          </div>     
-        </div>  
-
-
-        <div className="customers">
-          <div className="customer">
-            <div className="customer-name">
-             Random Drunk Guy
-            </div>
-            <div className="customer-score">
-              <div className="counter">
-                <button className="counter-action decrement">-</button>
-                  <div className="counter-score"> 11 </div>
-                <button className="counter-action increment">+</button>
+          <div className="customers">
+            <div className="customer">
+              <div className="customer-name">
+               Frá
               </div>
-            </div>
-            <div className="amount-field">
-              <div className="amount-value"> R$52,00</div>
-            </div>
-          </div>   
-        </div>  
+              <div className="customer-score">
+                <div className="counter">
+                  <button className="counter-action decrement">-</button>
+                    <div className="counter-score"> 31 </div>
+                  <button className="counter-action increment">+</button>
+                </div>           
+              </div>
+              <div className="amount-field">
+                <div className="amount-value"> R$43,00 </div>
+              </div>
+            </div>     
+          </div>  
 
 
-      </div>
+          <div className="customers">
+            <div className="customer">
+              <div className="customer-name">
+               Random Drunk Guy
+              </div>
+              <div className="customer-score">
+                <div className="counter">
+                  <button className="counter-action decrement">-</button>
+                    <div className="counter-score"> 11 </div>
+                  <button className="counter-action increment">+</button>
+                </div>
+              </div>
+              <div className="amount-field">
+                <div className="amount-value"> R$52,00</div>
+              </div>
+            </div>   
+          </div>  
+        </div> 
     );
   }
+}
+
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+    
+};
+
+App.defaultProps = {
+
+};
+
+function Header (props) {
+  return(
+    <div className="header">
+      <h1>{props.title}</h1>
+    </div>
+  );
 }
 
 export default App;
