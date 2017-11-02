9c89759e7b271ee63908bd002bbc82b012350115
Fraschetti <flaviofrasquetiferreira@fraschetti-3.local>
2017-11-02 10:46:11 -0200

    mockup finished

--- src/App.js
+++ src/App.js
@@ -1,18 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import {Grid, Jumbotron} from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="tab">
+        <div className="header">
+          <h1>Viella Tab App</h1>
+        </div>
+
+      <div className="total-container">
+        <div className="total-text">Total</div>  
+      </div>  
+
+        <div className="customers">
+          <div className="customer">
+            <div className="customer-name">
+             Frá
+            </div>
+            <div className="customer-score">
+              <div className="counter">
+                <button className="counter-action decrement">-</button>
+                  <div className="counter-score"> 31 </div>
+                <button className="counter-action increment">+</button>
+              </div>           
+            </div>
+            <div className="amount-field">
+              <div className="amount-value"> R$43,00</div>
+            </div>
+          </div>     
+        </div>  
+
+
+        <div className="customers">
+          <div className="customer">
+            <div className="customer-name">
+             Random Drunk Guy
+            </div>
+            <div className="customer-score">
+              <div className="counter">
+                <button className="counter-action decrement">-</button>
+                  <div className="counter-score"> 11 </div>
+                <button className="counter-action increment">+</button>
+              </div>
+            </div>
+            <div className="amount-field">
+              <div className="amount-value"> R$52,00</div>
+            </div>
+          </div>   
+        </div>  
+
+
       </div>
     );
   }
