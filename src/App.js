ad7da140e0f03e851b09cfe632dfcf58ec72bcf6
Fraschetti <flaviofrasquetiferreira@fraschetti-3.local>
2017-11-03 15:42:55 -0200

    functional state. Trying to solve arrays problem

--- src/App.js
+++ src/App.js
@@ -2,74 +2,60 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 // import {Grid, Jumbotron} from 'react-bootstrap';
 
+
 class App extends Component {
+
    constructor(props) {
     super(props);
   }
 
- 
+
+    CUSTOMERS = [
+
+    {
+      name : "Frá",
+      quantity: 15,
+      amount: 13.53,
+    },
+    {
+      name : "Frá",
+      quantity: 15,
+      amount: 13.53,
+    },  
+    
+  ]  
+
 
   render() {
     return (
       
-        <div className="tab">
-          
-          <Header title={this.props.title}/>
+      <div className="tab">
+        
+        <Header title={this.props.title}/>
 
-          <div className="total-container">
-            <div className="total-text">Total</div>  
-          </div>  
+        <div className="total-container">
+          <div className="total-text">Total</div>  
+        </div>  
 
-          <div className="customers">
-            <div className="customer">
-              <div className="customer-name">
-               Frá
-              </div>
-              <div className="customer-score">
-                <div className="counter">
-                  <button className="counter-action decrement">-</button>
-                    <div className="counter-score"> 31 </div>
-                  <button className="counter-action increment">+</button>
-                </div>           
-              </div>
-              <div className="amount-field">
-                <div className="amount-value"> R$43,00 </div>
-              </div>
-            </div>     
-          </div>  
-
-
-          <div className="customers">
-            <div className="customer">
-              <div className="customer-name">
-               Random Drunk Guy
-              </div>
-              <div className="customer-score">
-                <div className="counter">
-                  <button className="counter-action decrement">-</button>
-                    <div className="counter-score"> 11 </div>
-                  <button className="counter-action increment">+</button>
-                </div>
-              </div>
-              <div className="amount-field">
-                <div className="amount-value"> R$52,00</div>
-              </div>
-            </div>   
-          </div>  
-        </div> 
+        <div className="customers">
+          <Customer name="whatever" quantity={55} amount={12.51}/> 
+          <Customer name="whatever" quantity={55} amount={12.51}/> 
+        </div>   
+      </div> 
     );
   }
+
 }
 
+App.propTypes = {
+  customers: PropTypes.arrayOf(PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    quantity: PropTypes.number.isRequired,
+    amount: PropTypes.number.isRequired,
+  })).isRequired,
+  
+}
 
-Header.propTypes = {
-  title: PropTypes.string.isRequired,
-    
-};
-
-App.defaultProps = {
-
-};
 
 function Header (props) {
   return(
@@ -79,4 +65,66 @@ function Header (props) {
   );
 }
 
+Header.propTypes = {
+  title: PropTypes.string,   
+};
+
+Header.defaultProps = {
+  title: "Viella's Brew Tab"
+};
+
+
+function Customer(props){
+  return(
+  <div className="customer">
+    <div className="customer-name">
+     {props.name}
+    </div>
+    <div className="customer-score">
+      <Quantity quantity={props.quantity}/>            
+    </div>
+    <div className="amount-field">
+      <Amount amount={props.amount}/>
+    </div>
+  </div>  
+  );
+}
+
+Customer.propTypes = {
+  customers: PropTypes.arrayOf(PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    quantity: PropTypes.number.isRequired,
+    amount: PropTypes.number.isRequired,
+  })).isRequired,
+};
+
+
+function Quantity(props){
+  return(
+    <div className="counter">
+      <button className="counter-action decrement">-</button>
+        <div className="counter-score"> {props.quantity} </div>
+      <button className="counter-action increment">+</button>
+    </div>
+  );
+}
+
+Quantity.propTypes = {
+  quantity: PropTypes.number.isRequired,
+}
+
+
+function Amount(props){
+  return(
+      <div className="amount-value">R${props.amount} </div>
+  );
+}
+
+Amount.propTypes={
+    amount: PropTypes.number.isRequired,
+}
+
+
+
+ 
 export default App;
